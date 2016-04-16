1ee087ea319180749ef39389c6345b52c17214a0
Corecplusplustutor <corecplusplustutor@gmail.com>
2016-04-17 01:35:04 +0530

    Added files via upload

---
+++ bitwise_operator_testing_for _power_of_2_number.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std ;
+
+int main( )
+{
+   long long int n1;
+   cout<<"Enter an integer to test for power of 2 \n" ;
+   cin>> n1 ;
+
+   cin.ignore();
+
+   unsigned int r=n1^( n1 - 1 ) ;
+   if(( n1 & r ) == n1 )
+   {
+      cout<< n1 << " is a power of 2 ";
+   }
+   else cout<< n1 << " is not a power of 2 ";
+
+   cin.get() ;
+   return 0 ;
+}
